9c6b2fe14b4a0752a8c522a6fa085a3c6937e3b7
FranLopez <fco.jle@gmail.com>
2022-06-17 13:56:38 +0200

    scharp model onnx

---
+++ cSharp/ModelData.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.ML.Data;
+
+namespace Models
+{
+    public class OnnxModelInput
+    {
+        [ColumnName("input"), VectorType(12)]
+        public float[]? ModelInput { get; set; }
+    }
+
+    public class OnnxModelOutput
+    {
+        [ColumnName("label")]
+        public VBuffer<Int64> Label { get; set; }
+
+        [ColumnName("probabilities")]
+        public float[]? Probability { get; set; }
+    }
+
+}
---
+++ cSharp/OnnxModel.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.ML;
+
+namespace Models
+{
+    public class OnnxModel
+    {
+        private readonly MLContext _mlContext;
+        private readonly ITransformer _mlModel;
+
+        public OnnxModel(string modelFilePath)
+        {
+            _mlContext = new MLContext();
+            _mlModel = SetupModelTransformer(modelFilePath);
+        }
+
+        private ITransformer SetupModelTransformer(string modelPath)
+        {
+            var pipeline = _mlContext
+                .Transforms
+                .ApplyOnnxModel(modelPath);
+            ITransformer mlModel = pipeline.Fit(CreateEmptyDataView());
+            return mlModel;
+        }
+
+        private IDataView CreateEmptyDataView()
+        {
+            // Used to initialized a pretrained model.
+            List<OnnxModelInput> list = new() { new OnnxModelInput() { ModelInput = Array.Empty<float>() } };
+            return _mlContext.Data.LoadFromEnumerable(list);
+        }
+
+        public IEnumerable<OnnxModelOutput> Predict(List<OnnxModelInput> input)
+        {
+            IDataView dataView = _mlContext.Data.LoadFromEnumerable(input);
+            return Predict(dataView);
+        }
+
+        public IEnumerable<OnnxModelOutput> Predict(IDataView input)
+        {
+            var transformedValues = _mlModel.Transform(input);
+            var predictions = _mlContext.Data.CreateEnumerable<OnnxModelOutput>(transformedValues, reuseRowObject: true);
+            return predictions;
+        }
+    }
+}
+
+
